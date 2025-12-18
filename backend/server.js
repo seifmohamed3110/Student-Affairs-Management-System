a475f2c7604a4185745ce5add497777ba1a0edc2
mer486 <merola.g@gmail.com>
2025-12-18 21:05:44 +0200

    adding mongodb to codes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('❌ MongoDB connection error:', err.message);
+  });
+
+
 
 
 // Middleware to parse JSON
@@ -17,10 +30,9 @@ app.get('/', (req, res) => {
     res.send('Backend is working!');
 });
 
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
 });
-
 
 
 
